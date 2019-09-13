a880fb3dd880bb326c0c95ec48f93d8c3047a31e
alexander-chernyshev <alexander.a.chernyshev@gmail.com>
2019-09-13 18:51:25 +0300

    Created Graph directory

---
+++ Graph/AdjListGraph.cpp
@@ -0,0 +1,20 @@
+//
+// Created by alexander on 13.09.2019.
+//
+
+#include "AdjListGraph.h"
+
+AdjListGraph::AdjListGraph(size_t _vertex_count, bool _is_oriented)
+        : Graph(_vertex_count, _is_oriented) {
+    adj_list = std::vector<std::vector<Graph::Vertex>>(vertex_count);
+}
+
+void AdjListGraph::AddEdge(const Graph::Vertex &from, const Graph::Vertex &to) {
+    adj_list[from].push_back(to);
+    if (!is_oriented)
+        adj_list[to].push_back(from);
+}
+
+const std::vector<Graph::Vertex>& AdjListGraph::GetNeighbours(const Graph::Vertex &v) {
+    return adj_list[v];
+}
---
+++ Graph/Graph.h
@@ -0,0 +1,31 @@
+//
+// Created by alexander on 13.09.2019.
+//
+
+#ifndef ALGORITHMS3SEM_GRAPH_H
+#define ALGORITHMS3SEM_GRAPH_H
+
+#include <iostream>
+#include <vector>
+
+namespace GraphProcessing {
+    enum VertexMark{WHITE, GREY, BLACK};
+
+
+}
+
+class Graph {
+protected:
+    size_t vertex_count;
+    size_t edge_count = 0;
+    bool is_oriented;
+public:
+    typedef size_t Vertex;
+    Graph(size_t _vertex_count, bool _is_oriented);
+
+    virtual const std::vector<Vertex>& GetNeighbours(const Vertex& v) = 0;
+    virtual void AddEdge(const Vertex& from, const Vertex& to) = 0;
+};
+
+
+#endif //ALGORITHMS3SEM_GRAPH_H
