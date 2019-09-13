38bd8e198906dc06a1d212e548b13713b50f3e5f
alexander-chernyshev <alexander.a.chernyshev@gmail.com>
2019-09-13 18:40:29 +0300

    Added class Graph and inherited AdjListGraph

---
+++ Graph.cpp
@@ -0,0 +1,10 @@
+//
+// Created by alexander on 13.09.2019.
+//
+
+#include "Graph.h"
+
+Graph::Graph(size_t _vertex_count, bool _is_oriented) {
+    vertex_count = _vertex_count;
+    is_oriented = _is_oriented;
+}
