70740f918b3b5e6931f4456c5a33b88251a651a9
alexander-chernyshev <alexander.a.chernyshev@gmail.com>
2019-11-01 17:15:59 +0300

    Add Task O

--- contest/O.cpp
+++ contest/O.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <set>
+
 
 class DSU {
 private:
@@ -9,16 +8,19 @@ private:
     size_t set_count = 0;
     std::vector<size_t> parent;
     std::vector<size_t> rank;
+    std::vector<size_t> weight;
 public:
     explicit DSU(size_t _size) : size(_size) {
         parent.resize(size);
         rank.resize(size);
+        weight.resize(size);
     };
 
     void MakeSet(size_t v) {
         ++set_count;
         parent[v] = v;
         rank[v] = 0;
+        weight[v] = 0;
     }
 
     size_t FindSet(size_t v) {
@@ -28,7 +30,12 @@ public:
         return parent[v] = FindSet(parent[v]);
     }
 
-    void UnionSets(size_t a, size_t b) {
+    size_t GetWeight(size_t v) {
+        v = FindSet(v);
+        return weight[v];
+    }
+
+    void UnionSets(size_t a, size_t b, int edge_weight) {
         a = FindSet(a);
         b = FindSet(b);
         if (a != b) {
@@ -37,35 +44,46 @@ public:
                 std::swap(a, b);
             }
             parent[b] = a;
+            weight[a] += weight[b] + edge_weight;
             if (rank[a] == rank[b]) {
                 ++rank[a];
             }
+        } else {
+            weight[a] += edge_weight;
         }
     }
 
     size_t GetSetCount() const {
         return set_count;
     }
+
+    size_t GetSize() const {
+        return size;
+    }
 };
 
+
 int main() {
-    size_t vertex_count, edge_count;
-    std::cin >> vertex_count >> edge_count;
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    size_t vertex_count, query_count;
+    std::cin >> vertex_count >> query_count;
     DSU dsu(vertex_count);
     for (size_t i = 0; i < vertex_count; ++i) {
         dsu.MakeSet(i);
     }
-    if (dsu.GetSetCount() == 1) {
-        std::cout << 0;
-    } else {
-        for (size_t i = 0; i < edge_count; ++i) {
-            size_t from, to;
-            std::cin >> from >> to;
-            dsu.UnionSets(from, to);
-            if (dsu.GetSetCount() == 1) {
-                std::cout << i + 1;
-                break;
-            }
+    for (size_t i = 0; i < query_count; ++i) {
+        size_t query;
+        std::cin >> query;
+        if (query == 1) {
+            size_t x, y;
+            int w;
+            std::cin >> x >> y >> w;
+            dsu.UnionSets(x - 1, y - 1, w);
+        } else if (query == 2) {
+            size_t v;
+            std::cin >> v;
+            std::cout << dsu.GetWeight(v - 1) << '\n';
         }
     }
     return 0;
